1c9df582ef57e5c3fc0efff0b4427652ddd13630
Atheena Marie <22800058@usc.edu.ph>
2025-11-13 15:25:25 +0800

    frontend for login signup done

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Login from './pages/login';
+import Signup from './pages/signup';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path='/signup' element={<Signup />} />
+      <Route path="*" element={<Navigate to="/login" replace />} />
+    </Routes>
+  );
+}
+
+export default App;
---
+++ src/pages/signup.jsx
@@ -0,0 +1,95 @@
+import React, { useState } from 'react';
+import cover from '../assets/images/assorted.jpg';
+import logo from '../assets/images/BradPointsLogo.png';
+import { FaRegUser } from "react-icons/fa";
+import { MdOutlineEmail } from "react-icons/md";
+import { LuKeyRound } from 'react-icons/lu';
+import { Link } from 'react-router-dom';
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+
+const Signup = () => {
+
+  const [showPassword, setPassword] = useState(false);
+
+  return (
+    <>
+    <div className="min-h-screen flex items-center justify-center p-6">
+      <div className="flex flex-row items-center justify-center bg-white rounded-3xl h-140 max-w-4xl w-full shadow-2xl overflow-hidden border-l-12 border-orange-500">
+        <div className="flex flex-row w-full h-full">
+
+          {/* Left Side */}
+          <div className="relative w-2/5 h-full">
+            <img src={cover} alt="Braddex" className="object-cover w-auto h-full"/>
+          </div>
+
+          {/* Right Side */}
+          <div className="w-3/5 px-8 py-16 flex flex-col items-center justify-center">
+            <img src={logo} alt="BradPoints" className="w-16 h-16 object-cover mb-4" />
+            <h2 className="text-4xl font-bold mb-8 text-br-orange text-work-sans">Login</h2>
+
+            <form className='flex flex-col justify-center items-center w-full'>
+              <div className="mb-4 w-full flex flex-col justify-center items-center px-12 gap-6">
+
+                {/* username */}
+                <div className="relative w-full">
+                  <FaRegUser className="absolute left-4 top-1/2 transform -translate-y-1/2 text-br-gray text-lg" />
+                  <input
+                    type="text"
+                    id="username"
+                    name="email"
+                    placeholder="username"
+                    className="w-full pl-12 pr-4 py-2 border-2 border-gray-200 rounded-md focus:border-orange-500 focus:outline-none text-br-gray text-work-sans"
+                  />
+                </div>
+
+                {/* email */}
+                <div className="relative w-full">
+                  <MdOutlineEmail className="absolute left-4 top-1/2 transform -translate-y-1/2 text-br-gray text-xl" />
+                  <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    placeholder="email"
+                    className="w-full pl-12 pr-4 py-2 border-2 border-gray-200 rounded-md focus:border-orange-500 focus:outline-none text-br-gray text-work-sans"
+                  />
+                </div>
+
+                {/* password */}
+                <div className="relative w-full">
+                  <LuKeyRound className="absolute left-4 top-1/2 transform -translate-y-1/2 text-br-gray text-xl" />
+                  <input
+                    type= {showPassword ? "text" : "password"}
+                    id="password"
+                    name="password"
+                    placeholder="password"
+                    className="w-full pl-12 pr-4 py-2 border-2 border-gray-200 rounded-md focus:border-orange-500 focus:outline-none text-br-gray text-work-sans"
+                  />
+                  <button
+                    type='button'
+                    onClick={() => setPassword(!showPassword)}
+                    className='absolute right-4 top-1/2 transform -translate-y-1/2 text-br-gray text-xl'
+                  >
+                    {showPassword ? <FaEyeSlash /> : <FaEye />}
+                  </button>
+                </div>
+
+              </div>
+            </form>
+            
+            <div className='w-full px-12 mt-8 flex flex-col gap-6 justify-center items-center'>
+              <button className='flex items-center justify-center w-full py-2 text-work-sans font-bold text-white text-lg bg-br-orange rounded-lg'>
+                LOGIN
+              </button>
+              <p className='text-work-sans text-xs text-black'>Have an account already? 
+                <Link to="/login" className='text-br-orange hover:underline'> Login</Link>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    </>
+  );
+};
+
+export default Signup;
